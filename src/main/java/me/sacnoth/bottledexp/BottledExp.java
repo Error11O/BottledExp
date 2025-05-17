a8c73e806a270e69be7a9447da36673c027abf3a
error11O <matty.egan2112@gmail.com>
2025-05-18 00:22:11 +0100

    added pom.xml, added papermc
    TODO
    - replace permision dependecy with normal bukkit or full remove
    - fix plugin code itself (not many issues)

---
+++ src/main/java/me/sacnoth/bottledexp/BottledExp.java
@@ -0,0 +1,191 @@
+package me.sacnoth.bottledexp;
+
+import java.util.logging.Logger;
+
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BottledExp extends JavaPlugin {
+	static Logger log;
+	private BottledExpCommandExecutor myExecutor;
+	static int xpCost;
+	static int xpEarn;
+	static boolean usePermissions = false;
+	static boolean useVaultEcon = true;
+	static boolean useVaultPermissions = false;
+	static Permission vaultPermissions;
+	static String errAmount;
+	static String errXP;
+	static String errMoney;
+	static String langCurrentXP;
+	static String langOrder1;
+	static String langOrder2;
+	static String langRefund;
+	static String langItemConsumer;
+	static String langMoney;
+	static boolean settingUseItems;
+	static int settingConsumedItem;
+	static int amountConsumed;
+	static double moneyCost;
+	static Config config;
+	public static Economy economy = null;
+
+	public void onEnable() {
+		log = this.getLogger();
+
+		myExecutor = new BottledExpCommandExecutor(this);
+		getCommand("bottle").setExecutor(myExecutor);
+
+		getServer().getPluginManager()
+				.registerEvents(new EventListener(), this);
+
+		config = new Config(this);
+		config.load();
+
+		if (!setupEconomy()) {
+			log.info("Vault not found - Disabeling economy capabilities.");
+			useVaultEcon = false;
+		}
+
+		log.info("You are now able to fill XP into Bottles");
+		if (Bukkit.getServer().getPluginManager().isPluginEnabled("Vault")) {
+			setupPermissions();
+			useVaultPermissions = true;
+			log.info("Using " + vaultPermissions.getName() + " via Vault.");
+		} else {
+			log.warning("Neither PEX nor Vault found, BottledExp will not work properly!");
+		}
+	}
+
+	public void onDisable() {
+		log.info("You are no longer able to fill XP into Bottles");
+	}
+	
+	private boolean setupPermissions() {
+		RegisteredServiceProvider<Permission> permissionProvider = getServer()
+				.getServicesManager().getRegistration(
+						net.milkbowl.vault.permission.Permission.class);
+		if (permissionProvider != null) {
+			vaultPermissions = permissionProvider.getProvider();
+		}
+		return (vaultPermissions != null);
+	}
+
+	public static boolean checkPermission(String node, Player player) {
+		if (usePermissions) {
+			player.sendMessage(ChatColor.RED
+					+ "You don't have permission to do this!");
+			return false;
+		} else if (useVaultPermissions && vaultPermissions.isEnabled()) {
+			if (vaultPermissions.playerHas(player.getWorld(), player.getName(),
+					node)) {
+				return true;
+			}
+			player.sendMessage(ChatColor.RED
+					+ "You don't have permission to do this!");
+			return false;
+		}
+		player.sendMessage(ChatColor.RED
+				+ "Neither PEX nor Vault found, BottledExp will not work properly!");
+		return false;
+	}
+
+	public static int levelToExp(int level) {
+		if (level <= 15)
+		{
+			return 17 * level;
+		}
+		else if (level <= 30)
+		{
+			return (3*level*level/2)-(59*level/2)+360;
+		}
+		else
+		{
+			return (7*level*level/2)-(303*level/2)+2220;
+		}
+	}
+
+	public static int deltaLevelToExp(int level) {
+		if (level <= 15)
+		{
+			return 17;
+		}
+		else if (level <= 30)
+		{
+			return 3 * level - 31;
+		}
+ else {
+			return 7 * level - 155;
+		}
+	}
+
+	public static int getPlayerExperience(Player player) {
+		int bukkitExp = player.getTotalExperience();
+		return bukkitExp;
+	}
+
+	public static boolean checkInventory(Player player, Material item, int amount) {
+		PlayerInventory inventory = player.getInventory();
+
+		if (inventory.contains(Material.GLASS_BOTTLE, amount)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean consumeItem(Player player, Material item, int amount) {
+		PlayerInventory inventory = player.getInventory();
+
+		if (inventory.contains(Material.GLASS_BOTTLE, amount)) {
+			ItemStack items = new ItemStack(item, amount);
+			inventory.removeItem(items);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static int countItems(Player player, Material item) {
+		PlayerInventory inventory = player.getInventory();
+
+		int amount = 0;
+		ItemStack curItem;
+		for (int slot = 0; slot < inventory.getSize(); slot++) {
+			curItem = inventory.getItem(slot);
+			if (curItem != null && curItem.getType() == item)
+				amount += curItem.getAmount();
+		}
+		return amount;
+	}
+
+	private boolean setupEconomy() {
+		if (getServer().getPluginManager().getPlugin("Vault") == null) {
+			return false;
+		}
+		RegisteredServiceProvider<Economy> rsp = getServer()
+				.getServicesManager().getRegistration(Economy.class);
+		if (rsp == null) {
+			return false;
+		}
+		economy = rsp.getProvider();
+		return economy != null;
+	}
+
+	public static double getBalance(Player player) {
+		return BottledExp.economy.getBalance(player.getName());
+	}
+
+	public static void withdrawMoney(Player player, double price) {
+		BottledExp.economy.withdrawPlayer(player.getName(), price);
+	}
+}
---
+++ src/main/java/me/sacnoth/bottledexp/BottledExpCommandExecutor.java
@@ -0,0 +1,140 @@
+package me.sacnoth.bottledexp;
+
+import java.util.HashMap;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.jetbrains.annotations.NotNull;
+
+public class BottledExpCommandExecutor implements CommandExecutor {
+
+	public BottledExpCommandExecutor(BottledExp plugin) {
+	}
+
+	@Override
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd,
+							 @NotNull String commandLabel, String[] args) {
+		if ((sender instanceof Player player)) {
+            if (cmd.getName().equalsIgnoreCase("bottle")
+					&& BottledExp.checkPermission("bottle.use", player)) {
+				int currentxp = BottledExp.getPlayerExperience(player);
+
+				if (args.length == 0) {
+					sender.sendMessage(BottledExp.langCurrentXP + ": " + currentxp + " XP!");
+					return true;
+				}
+				int amount = 0;
+				if (args.length == 1) {
+					switch (args[0]) {
+						case "max":
+							if (!BottledExp.checkPermission("bottle.max", player)) return false;
+							amount = (int) Math.floor((double) currentxp / BottledExp.xpCost);
+							amount = Math.min(BottledExp.countItems(player, Material.GLASS_BOTTLE) / BottledExp.amountConsumed, amount);
+							break;
+						case "reload":
+							if (!BottledExp.checkPermission("bottle.reload", player)) return false;
+							BottledExp.config.reload(sender);
+							sender.sendMessage(ChatColor.GREEN + "Config reloaded!");
+							break;
+						default:
+							try {
+								amount = Integer.valueOf(args[0]).intValue();
+							} catch (NumberFormatException nfe) {
+								sender.sendMessage(ChatColor.RED
+										+ BottledExp.errAmount);
+								return false;
+							}
+							break;
+					}
+
+					if (currentxp < amount * BottledExp.xpCost) {
+						sender.sendMessage(ChatColor.RED + BottledExp.errXP);
+						return true;
+					} else if (amount <= 0) {
+						amount = 0;
+						sender.sendMessage(BottledExp.langOrder1 + " " + amount
+								+ " " + BottledExp.langOrder2);
+						return true;
+					}
+
+					boolean money = false;
+					if (BottledExp.useVaultEcon) // Check if the player has enough
+												// money
+					{
+						if (BottledExp.getBalance(player) > BottledExp.moneyCost
+								* amount) {
+							money = true;
+						} else {
+							player.sendMessage(BottledExp.errMoney);
+							return true;
+						}
+					}
+
+					boolean consumeItems = false;
+					if (BottledExp.settingUseItems) // Check if the player has
+													// enough items
+					{
+						consumeItems = BottledExp.checkInventory(player,
+								Material.GLASS_BOTTLE, amount
+										* BottledExp.amountConsumed);
+						if (!consumeItems) {
+							sender.sendMessage(ChatColor.RED
+									+ BottledExp.langItemConsumer);
+							return true;
+						}
+					}
+
+					PlayerInventory inventory = player.getInventory();
+					ItemStack items = new ItemStack(Material.EXPERIENCE_BOTTLE, amount);
+					HashMap<Integer, ItemStack> leftoverItems = inventory
+							.addItem(items);
+					player.setTotalExperience(0);
+					player.setLevel(0);
+					player.setExp(0);
+					player.giveExp(currentxp - (amount * BottledExp.xpCost));
+					if (leftoverItems.containsKey(0)) {
+						int refundAmount = leftoverItems.get(0).getAmount();
+						player.giveExp(refundAmount * BottledExp.xpCost);
+						player.sendMessage(BottledExp.langRefund + ": "
+								+ refundAmount);
+						amount -= refundAmount;
+					}
+
+					if (money) // Remove money from player
+					{
+						BottledExp.withdrawMoney(player, BottledExp.moneyCost
+								* amount);
+						player.sendMessage(BottledExp.langMoney + ": "
+								+ BottledExp.moneyCost * amount + " "
+								+ BottledExp.economy.currencyNamePlural());
+					}
+
+					if (consumeItems) // Remove items from Player
+					{
+						if (!BottledExp.consumeItem(player,
+								Material.GLASS_BOTTLE, amount
+										* BottledExp.amountConsumed)) {
+							sender.sendMessage(ChatColor.RED
+									+ BottledExp.langItemConsumer);
+							return true;
+						}
+					}
+
+					sender.sendMessage(BottledExp.langOrder1 + " " + amount
+							+ " " + BottledExp.langOrder2);
+				}
+				return true;
+			}
+		} else {
+			sender.sendMessage(ChatColor.RED + "You have to be a player!");
+			return false;
+		}
+		return false;
+	}
+}
